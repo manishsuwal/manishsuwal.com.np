3fc9ebcef53f0fdbb594641ed8678c43634a5883
Manish Suwal <enwil410@gmail.com>
2013-12-22 17:25:59 +0545

    manishsuwal.com.np as on 20131222

---
+++ page_manishsuwal.php
@@ -0,0 +1,219 @@
+<?php
+/*
+Template Name: Manish Suwal
+*/
+?>
+
+
+<html>
+<head>
+<meta name="description" content="Manish Suwal's Personal Bio"/>
+<link rel="author" href="https://plus.google.com/u/0/118039990517392424384"/>
+<title>Manish Suwal 'Enwil'</title>
+<style type='text/css'>
+	body {
+		background: url('http://manishsuwal.com.np/wp-content/uploads/2013/12/manishsuwal_background.jpg') top left no-repeat;
+		background-size: cover;
+		background-attachment: fixed;
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		padding: 0;
+	}
+
+	#bar {
+		width: 300px;
+		min-height: 100%;
+		position: absolute;
+		left: 0;
+		background: rgba(0,0,0,.75) url('http://manishsuwal.com.np/wp-content/uploads/2013/12/manishsuwal_logo.png') top center no-repeat;
+		color: #eee;
+		padding: 0 20px;
+		font-family: helvetica, verdana, arial, sans-serif;
+		line-height: 1.3em;
+		box-shadow: -4px 0px 40px rgba(0,0,0,.25);
+	}
+
+	.profile {
+		margin-top: 100px;
+	}
+
+	.box {
+		margin-bottom: 15px;
+		padding-bottom: 15px;
+		border-bottom: 1px solid #444;
+	}
+
+	.single {
+		margin-top: 1em;
+		margin-bottom: .5em;
+		font-weight: bold;
+		font-size: 1.2em;
+		line-height: 1.3em;
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+		list-style-type: none;
+	}
+
+	.explain, ul {
+		font-size: 1em;
+		color: #bbb;
+		margin-left: 10px;
+	}
+
+	.links.box {
+		border-bottom: none;
+		padding-bottom: none;
+	}
+
+	a {
+		color: #bdf;
+		text-decoration: none;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+
+	#title{
+		font-size: 2.4em;
+		font-weight: 900;
+		color: #ccc;
+
+	}
+
+	.hr {
+		margin-bottom: 15px;
+		padding-bottom: 15px;
+		border-bottom: 1px solid #444;
+	}
+
+
+</style>
+</head>
+<body>
+<div id='bar'>
+	<div class='profile box'>
+
+		<div class='single'>Co-Founder of DevotePress</div>
+		<div class='explain'><a href='http://devotepress.com' target="_blank">DevotePress</a> is an awesome tutorial based website solely dedicated to WordPress. It provides quality tutorials, tips, tricks & hack to help all level of WordPress users - beginners to programmers.</div>
+
+		<div class='single'>WordPress Theme Developer</div>
+		<div class='explain'>I have developed <a href="http://enwil.com/willingness" target="_blank">willingness</a> and <a href="http://enwil.com/enormous" target="_blank">enormous</a> WordPress themes. Willingness is available on <a href="http://wordpress.org/themes/willingness" target="_blank">WordPress theme directory</a></div>
+
+		<div class='single'>Contributor at Independent Publisher</div>
+		<div class='explain'><a href='http://independentpublisher.me' target="_blank">Independent Publisher</a> is a beautiful reader-focused WordPress theme designed for independent writers.</div>
+
+		<div class='single'>Blogger at <a href='http://enwil.com'>Enwil.com</a></div>
+		<div class='explain'>
+			I started blogging on 2012 July to track myself progress overtime. I post once a week - mostly about practical philosophy and life.
+
+			I have been <a href="http://blog.nepal-travel.org/nepali-blogger-enwil-interview/" target="_blank">featured and interviewed on Nepal Travel</a> as a blogger.
+		</div>
+<div class='hr'></div>
+
+	<div class='learning box'>
+		<div class='single'>Things I'm Learning</div>
+		<ul>
+			<li>Git</li>
+			<li>Programming</li>
+			<li>Spanish</li>
+			<li>Social Dynamics</li>
+		</ul>
+	</div>
+	<div class='excited box'>
+		<div class='single'>Things I'm Excited About</div>
+		<ul>
+			<li>Minimalism</li>
+			<li>Stoicism</li>
+			<li>Philosophy</li>
+			<li>Self-Discipline</li>
+			<li>Self-Education</li>
+			<li><a href="http://enwil.com/dvorak" target="_blank">Dvorak</a></li>
+			<li>Dotsies</li>
+			<li>Website building</li>
+			<li>Meeting old friends</li>
+			<li>Working hard</li>
+			<li>Rapport building</li>
+		</ul>
+	</div>
+	<div class='favorite box'>
+		<div class='single'>Favorite Books</div>
+		<div class='explain'><a href='http://www.amazon.com/gp/product/B0040JHNQG/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=B0040JHNQG&linkCode=as2&tag=enwil-20' target='_blank'>A Guide to the Good Life: The Ancient Art of Stoic Joy</a></div>
+		<br>
+		<div class='explain'><a href='http://www.amazon.com/gp/product/B00873W3X4/ref=as_li_ss_tl?ie=UTF8&tag=enwil-20&linkCode=as2&camp=1789&creative=390957&creativeASIN=B00873W3X4' target='_blank'>Freedom From the Known</a></div>
+		<br>
+		<div class='explain'><a href='http://www.amazon.com/gp/product/B007A4SDCG/ref=as_li_ss_tl?ie=UTF8&tag=enwil-20&linkCode=as2&camp=1789&creative=390957&creativeASIN=B007A4SDCG' target='_blank'>The War of Art</a></div>
+		<br>
+		<div class='explain'><a href='http://www.amazon.com/gp/product/B005P2A6L6/ref=as_li_ss_tl?ie=UTF8&tag=enwil-20&linkCode=as2&camp=1789&creative=390957&creativeASIN=B005P2A6L6' target='_blank'>The Five Secrets You Must Discover Before You Die</a></div>
+		<br>
+		<div class='explain'><a href='http://www.amazon.com/gp/product/B009UXBATS/ref=as_li_ss_tl?ie=UTF8&tag=enwil-20&linkCode=as2&camp=1789&creative=390957&creativeASIN=B009UXBATS' target='_blank'>Disrupting the Rabblement</a></div>
+
+
+	</div>
+	<div class='links box'>
+		<div class='single'>Links to My Stuff</div>
+		<ul>
+		<li><a href='http://facebook.com/manishsuwal' target="_blank">Facebook a/c</a></li>
+		<li><a href='https://www.facebook.com/pages/Enwilcom/266258376816540' target='_blank'>Facebook page</a></li>
+		<li><a href='http://twitter.com/manishsuwal' target="_blank">Twitter</a></li>
+		<li><a href='https://plus.google.com/u/0/118039990517392424384' target='_blank'>Google+</a></li>
+		<li><a href='http://instagram.com/manishsuwal' target="_blank">Instagram</a></li>
+		<li><a href='http://enwil.com' target="_blank">Blog</a></li>
+		<li><a href='http://enwil.com/about' target="_blank">About Page</a></li>
+		<li><a href='http://github.com/manishsuwal' target="_blank">GitHub</a></li>
+		<li><a href='mailto:manish@enwil.com'>Email</a></li>
+	</ul>
+	</div>
+	<div class='interesting box'>
+		<div class='single'>Random Facts About Me</div>
+		<ul>
+			<li>Instead of QWERTY, I use <a href="http://enwil.com/dvorak" target="_blank">Dvorak</a> keyboard layout to type</li>
+			<br>
+			<li>I don't watch TV</li>
+			<br>
+			<li>I don't play any type of computer or video games</li>
+			<br>
+			<li><a href="http://blog.seanbonner.com/2010/02/01/ive-given-up-using-soap/" target="_blank">I don't use soap or shampoo while bathing</a></li>
+			<br>
+			<li>I can read <a href="http://dotsies.org" target='_blank'>dotsies</a></li>
+			<br>
+			<li>My most popular blog post is <a href="http://enwil.com/aghori-baba" target="_blank">Lessons From Aghori Baba (Why It's Dangerous to Know Your Future)</a></li>
+			<br>
+			<li>I hate chocolates and sugar. I quit sugar.</li>
+			<br>
+			<li>I used to hate and fail at computer, now all I do is program at computer</li>
+			<br>
+			<li>I used to hate writing, now I write everyday and <a href="http://enwil.com" target='_blank'>blog weekly</a></li>
+			<br>
+			<li>I write for <a href='http://enwil.com/wp-content/uploads/2013/09/CHIC-Fashion-Magazine-Fashion-Blogging.png' target='_blank'>CHIC magazine</a></li>
+			<br>
+			<li>I have been mentioned on Niall's book, <a href='http://www.amazon.com/gp/product/B009UXBATS/ref=as_li_ss_tl?ie=UTF8&tag=enwil-20&linkCode=as2&camp=1789&creative=390957&creativeASIN=B009UXBATS' target='_blank'>Disrupting the Rabblement</a></li>
+			<br>
+			<li>I've made 30 websites till date + counting. 9 on 2012 and rest on 2013.</li>
+			<br>
+			<li>I've revamped TED X speaker's website</li>
+			<br>
+			<li>My most influential person on the internet 'Niall Doherty' is my friend now.</li>
+		</ul>
+	</div>
+	<div class='interesting box'>
+		<div class='single'>Projects Work/Personal</div>
+		<ul>
+			<li><a href='http://github.com/manishsuwal/willingness' target='_blank'>Willingness WordPress theme</a></li>
+			<li><a href='http://github.com/manishsuwal/enormous' target='_blank'>Enormous WordPress theme</a></li>
+			<li><a href='http://github.com/manishsuwal/independent-publisher' target='_blank'>Independent-Publisher WordPress theme</a></li>
+			<li><a href='http://devotepress.com/' target='_blank'>DevotePress</a></li>
+			<li><a href='http://enwil.com/' target='_blank'>Blogging at Enwil.com</a></li>
+      <li><a href='http://travelbloggingcalendar.com/' target='_blank'>Travel Blogging Calendar</a></li>
+			<li>One Second Everyday</li>
+			<li>750 words everyday</li>
+			<li>Sleep Tracking</li>
+			<li>Gratitude Tracking</li>
+	</div>
+</div>
+</body>
+</html>
